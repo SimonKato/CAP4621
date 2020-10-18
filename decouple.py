0fc688fac61363aba45a9c82ce3adf3369e22985
Mochi <mochiko@mochi.com>
2020-10-18 18:30:08 -0400

    add arguments to decouple

--- decouple.py
+++ decouple.py
@@ -1,9 +1,19 @@
 import os
 import shutil
+import sys
 
+#get arguments
+args = sys.argv
+if len(args) != 3:
+    raise Exception("Structure: python decouple.py in")
+
+masked = args[1]
+unmasked = args[2]
 counter = 0
+print(masked)
+print(unmasked)
 
-for root, subDir, files in os.walk("/content/pytorch-CycleGAN-and-pix2pix/datasets/cloned-repo-masked"):
+for root, subDir, files in os.walk(masked):
     if files:
         for file in files:
             shutil.copyfile(root + '/' + file, "/content/pytorch-CycleGAN-and-pix2pix/datasets/cap_dataset/masked/" + str(counter) + file)
@@ -12,7 +22,7 @@ for root, subDir, files in os.walk("/content/pytorch-CycleGAN-and-pix2pix/datase
         continue
 
 counter = 0
-for root, subDir, files in os.walk("/content/pytorch-CycleGAN-and-pix2pix/datasets/cloned-repo-unmasked"):
+for root, subDir, files in os.walk(unmasked):
     if files:
         for file in files:
             shutil.copyfile(root + '/' + file, "/content/pytorch-CycleGAN-and-pix2pix/datasets/cap_dataset/unmasked/" + str(counter) + file)
